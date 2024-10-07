2582e5cdfb0ed7236d3a995098a6aa5174a8ec40
ryanxx37 <ryanxx37@gmail.com>
2024-10-07 19:30:44 -0400

    Updata style  (issue 5)

--- hw3.php
+++ hw3.php
@@ -22,7 +22,6 @@ if (isset($_POST['cell']) && !isset($_SESSION['winner'])) {
         $_SESSION['board'][$cell] = $_SESSION['turn'];
         updateSessionBoard(); 
 
-
         $winner = whoIsWinner();
         if ($winner != null) {
             $_SESSION['winner'] = $winner;
@@ -41,7 +40,6 @@ function fillX() {
     $_SESSION['board'] = array_fill(0, 9, ''); 
     $_SESSION['turn'] = 'X';
 }
-
 
 function updateSessionBoard() {
     for ($i = 0; $i < 9; $i++) {
@@ -69,48 +67,58 @@ function message() {
     <meta charset="UTF-8">
     <title>Tic Tac Toe</title>
     <style>
-        table {
-            margin: 20px auto;
-        }
+		/* Button background is blue with a black border*/
+		button {
+			background-color: #3498db;
+			height: 100%;
+			width: 100%;
+			text-align: center;
+			font-size: 20px;
+			color: white;
+			vertical-align: middle;
+			border: 0px;
+		}
 
-        td {
-            width: 50px;
-            height: 50px;
-            text-align: center;
-            vertical-align: middle;
-        }
+		/* Styles the table cells to look like a tic-tac-toe grid */
+		table td {
+			text-align: center;
+			vertical-align: middle;
+			padding: 0px;
+			margin: 0px;
+			width: 75px;
+			height: 75px;
+			font-size: 20px;
+			border: 3px solid #040404;
+			color: white;
+		}
 
-        .X {
-            background-color: green;
-        }
-
-        .O {
-            background-color: red;
-        }
-
-        button {
-            width: 100%;
-            height: 100%;
-            background-color: transparent;
-            border: none;
-        }
+		/* This shows a darker blue background when the mouse hovers over the buttons */
+		button:hover,
+		input[type="submit"]:hover,
+		button:focus,
+		input[type="submit"]:focus {
+			background-color: #04469d;
+			text-decoration: none;
+			outline: none;
+		}
     </style>
 </head>
 <body>
 
-    <h1 style="text-align:center;">Tic Tac Toe</h1>
+    <h1>Tic Tac Toe</h1>
 
-    <div  style="text-align:center;"><?php message() ?></div>
+    <?php message() ?>
 
     <table>
         <?php for ($row = 0; $row < 3; $row++): ?>
             <tr>
                 <?php for ($col = 0; $col < 3; $col++): 
                     $cellIndex = $row * 3 + $col; ?>
-                    <td class="<?php echo $_SESSION['board'][$cellIndex]; ?>">
+                    <td style="background-color: 
+                    <?php echo $_SESSION['board'][$cellIndex] == 'X' ? 'green' : ($_SESSION['board'][$cellIndex] == 'O' ? 'red' : ''); ?>; color: black;">
                         <?php if ($_SESSION['board'][$cellIndex] == ''): ?>
                             <?php if (!isset($_SESSION['winner']) || $_SESSION['winner'] == ''): ?>
-                                <form method="post">
+                                <form method="post" style="height: 100%; width: 100%; padding: 0; margin: 0;">
                                     <input type="hidden" name="cell" value="<?php echo $cellIndex; ?>">
                                     <button type="submit"><?php echo $_SESSION['turn']; ?></button>
                                 </form>
@@ -124,10 +132,9 @@ function message() {
         <?php endfor; ?>
     </table>
 
-    <div  style="text-align:center;">
-        <form method="post">
-            <button type="submit" name="reset">Reset Game</button>
-        </form>
-    </div>
+    <form method="post">
+        <button type="submit" name="reset">Reset Game</button>
+    </form>
+
 </body>
 </html>
